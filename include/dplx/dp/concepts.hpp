7cd1066c803d81a615932033c19d954713f69532
Henrik S. Gaßmann <henrik@gassmann.onl>
2020-04-06 07:38:06 +0200

    Add basic encoder concepts
    Add the output_stream concept describing types that can be written to by
    an encoder. The concept will be enhanced with an outcome v2 error
    reporting mechanism.
    
    Add the encodeable concept describing types which can be encoded to an
    output_stream.
    
    - Add a concept for _actual_ (built-in) integer types.
    - Add a concept for iec559 (built-in) floating point types.

---
+++ include/dplx/dp/concepts.hpp
@@ -0,0 +1,72 @@
+
+// Copyright Henrik Steffen Gaßmann 2020
+//
+// Distributed under the Boost Software License, Version 1.0.
+//         (See accompanying file LICENSE or copy at
+//           https://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <concepts>
+#include <limits>
+#include <ranges>
+#include <type_traits>
+
+namespace dplx::dp::detail
+{
+
+template <typename T, typename... Ts>
+concept any_of = (... || std::is_same_v<T, Ts>);
+
+template <typename T, typename... Ts>
+concept none_of = (... && !std::is_same_v<T, Ts>);
+
+}
+
+namespace dplx::dp
+{
+
+template <typename T>
+concept integer = std::integral<T> &&detail::none_of<std::remove_cv_t<T>,
+                                                     bool,
+                                                     char,
+                                                     wchar_t,
+                                                     char8_t,
+                                                     char16_t,
+                                                     char32_t>;
+
+template <typename T>
+concept iec559_floating_point = std::is_floating_point_v<T> &&
+                                    std::numeric_limits<T>::is_iec559 &&
+                                (sizeof(T) == 4 || sizeof(T) == 8);
+
+// clang-format off
+template <typename T>
+concept output_stream
+    = requires(T &stream, typename T::write_proxy proxy, std::size_t const n)
+    {
+        typename T::write_proxy;
+        requires std::ranges::contiguous_range<typename T::write_proxy>;
+        requires std::same_as<
+            std::ranges::range_value_t<typename T::write_proxy>,
+            std::byte>;
+        {stream.write(n)} -> typename T::write_proxy;
+        proxy.shrink(n);
+    };
+// clang-format on
+
+template <output_stream Stream, typename T>
+class basic_encoder;
+
+// clang-format off
+template <typename Stream, typename T>
+concept encodeable                            
+    = output_stream<Stream> &&
+        requires(Stream &ctx, T &&t)
+        {
+            typename basic_encoder<Stream, T>;
+            basic_encoder<Stream, T>{ctx}(std::forward<T>(t));
+        };
+// clang-format on
+
+} // namespace dplx::dp
