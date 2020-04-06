4659ed8ab9578cc85dcc2c41396b0dc1775f3a19
Henrik S. Gaßmann <henrik@gassmann.onl>
2020-04-06 07:00:14 +0200

    Add boost test std::byte formatter
    - Add fmt dependency to the test project.
    - Fix warning configuration.

--- tests/boost-test.hpp
+++ tests/boost-test.hpp
@@ -7,6 +7,21 @@
 
 #pragma once
 
+#include <cstddef>
+
+#include <fmt/printf.h>
+
+namespace boost::test_tools::tt_detail::impl
+{
+inline auto boost_test_print_type(std::ostream &s, std::byte b)
+    -> std::ostream &
+{
+    fmt::print(s, FMT_STRING("{:x}"), static_cast<std::uint8_t>(b));
+    return s;
+}
+} // namespace boost::test_tools::tt_detail::impl
+
+
 #include <boost/predef.h>
 
 #if defined BOOST_COMP_GNUC_AVAILABLE
@@ -26,7 +41,6 @@
 
 #include <boost/test/data/monomorphic.hpp>
 #include <boost/test/data/test_case.hpp>
-#include <boost/test/parameterized_test.hpp>
 
 #if defined BOOST_COMP_MSVC_AVAILABLE
 #pragma warning(pop)
