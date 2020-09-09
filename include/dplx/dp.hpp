31a0cb963dac64fbd95a5e940e515929d02a8188
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2020-09-09 13:56:42 +0200

    Add master include header

---
+++ include/dplx/dp.hpp
@@ -0,0 +1,19 @@
+
+// Copyright Henrik Steffen Gaßmann 2020.
+//
+// Distributed under the Boost Software License, Version 1.0.
+//         (See accompanying file LICENSE or copy at
+//           https://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <dplx/dp/disappointment.hpp>
+#include <dplx/dp/fwd.hpp>
+
+#include <dplx/dp/decoder/api.hpp>
+#include <dplx/dp/decoder/core.hpp>
+#include <dplx/dp/decoder/std_container.hpp>
+#include <dplx/dp/decoder/std_string.hpp>
+
+#include <dplx/dp/encoder/api.hpp>
+#include <dplx/dp/encoder/core.hpp>
