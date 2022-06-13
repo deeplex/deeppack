e0d2fdd69772c44749a94c3a5d5feb498acb5114
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2022-06-13 19:02:16 +0200

    Reset to copier deeplex C++ library template
    - Add a test TU for every header.
    - Move to merged source layout.
    - Add docs skeleton.
    - Add github issue and PR templates.

---
+++ docs/conf.py
@@ -0,0 +1,78 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'deeppack'
+copyright = '2020, Henrik Steffen Gaßmann'
+author = 'Henrik Steffen Gaßmann'
+
+# The full version, including alpha/beta/rc tags
+release = '0.0.0-alpha.2'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx_multiversion",
+]
+primary_domain = 'cpp'
+highlight_language = 'cpp'
+cpp_index_common_prefix = [
+    'dplx::dp',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = [
+    '_templates',
+]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+smv_tag_whitelist = None
+smv_branch_whitelist = "^(master|release/v.+)$"
+smv_remote_whitelist = "^origin$"
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'furo'
+html_theme_options = {
+    "light_css_variables": {
+        "font-stack--monospace": "Fira Code, Cascadia Code, \"SFMono-Regular\", Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace",
+    },
+    "dark_css_variables": {
+        "font-stack--monospace": "Fira Code, Cascadia Code, \"SFMono-Regular\", Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace",
+    },
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+html_css_files = [
+    'css/custom.css',
+]
---
+++ sources.cmake
@@ -0,0 +1,101 @@
+
+dplx_target_sources(deeppack
+    TEST_TARGET deeppack-tests
+    MODE SMART_SOURCE MERGED_LAYOUT
+    BASE_DIR dplx
+    
+    PUBLIC
+        dp
+        dp/disappointment
+)
+
+dplx_target_sources(deeppack
+    TEST_TARGET deeppack-tests
+    MODE SMART_HEADER_ONLY MERGED_LAYOUT
+    BASE_DIR dplx
+
+    PUBLIC
+        dp/config
+
+        dp/concepts
+        dp/fwd
+        dp/layout_descriptor
+        dp/memory_buffer
+        dp/object_def
+        dp/stream
+        dp/tuple_def
+        dp/type_code
+
+        dp/detail/item_size
+        dp/encoder/api
+        dp/encoder/arg_list
+        dp/encoder/chrono
+        dp/encoder/core
+        dp/encoder/narrow_strings
+        dp/encoder/object_utils
+        dp/encoder/std_path
+        dp/encoder/tuple_utils
+        dp/item_emitter
+
+        dp/decoder/api
+        dp/decoder/chrono
+        dp/decoder/core
+        dp/decoder/object_utils
+        dp/decoder/std_container
+        dp/decoder/std_path
+        dp/decoder/std_string
+        dp/decoder/tuple_utils
+        dp/decoder/utils
+        dp/detail/parse_item
+        dp/item_parser
+        dp/skip_item
+
+        dp/customization
+        dp/customization.std
+        dp/indefinite_range
+        dp/map_pair
+        dp/tag_invoke
+
+        dp/streams/chunked_input_stream
+        dp/streams/chunked_output_stream
+        dp/streams/memory_input_stream
+        dp/streams/memory_output_stream
+
+        dp/detail/bit
+        dp/detail/hash
+        dp/detail/mp_for_dots
+        dp/detail/perfect_hash
+        dp/detail/type_utils
+        dp/detail/utils
+        dp/detail/workaround
+)
+
+file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/generated/src/dplx/dp/detail)
+configure_file(tools/config.hpp.in ${CMAKE_CURRENT_BINARY_DIR}/generated/src/dplx/dp/detail/config.hpp @ONLY)
+target_sources(deeppack PUBLIC $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/generated/src/dplx/dp/detail/config.hpp>)
+
+if (BUILD_TESTING)
+    dplx_target_sources(deeppack-tests PRIVATE
+        MODE VERBATIM
+        BASE_DIR dp_tests
+
+        PRIVATE
+            test_utils.hpp
+    )
+
+    target_sources(deeppack-tests PRIVATE
+        tests/encoder.blob.test.cpp
+        tests/encoder.map.test.cpp
+        tests/encoder.range.test.cpp
+        tests/encoder.string.test.cpp
+        tests/encoder.tuple.test.cpp
+
+        tests/enum_codec.test.cpp
+
+        tests/item_emitter.integer.test.cpp
+        tests/item_parser.array.test.cpp
+        tests/item_parser.binary.test.cpp
+        tests/item_parser.expect.test.cpp
+        tests/item_parser.integer.test.cpp
+    )
+endif ()
