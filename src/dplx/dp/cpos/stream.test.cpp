be207ab73e5080f367fdb346e41257b21be76299
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2023-02-03 14:05:01 +0100

    feat: Treat types derived from `*_buffer` as streams

--- src/dplx/dp/cpos/stream.test.cpp
+++ src/dplx/dp/cpos/stream.test.cpp
@@ -9,11 +9,92 @@
 
 #include <catch2/catch_test_macros.hpp>
 
+#include <dplx/dp/api.hpp>
+#include <dplx/dp/codecs/core.hpp>
+#include <dplx/dp/streams/input_buffer.hpp>
+#include <dplx/dp/streams/output_buffer.hpp>
+
 #include "test_utils.hpp"
 
 namespace dp_tests
 {
 
-// TODO: Add tests for stream CPOs
+namespace
+{
+
+class movable_stream final
+    : public dp::input_buffer
+    , public dp::output_buffer
+{
+public:
+    ~movable_stream() noexcept = default;
+    movable_stream() noexcept = default;
+
+    movable_stream(movable_stream const &) noexcept = delete;
+    auto operator=(movable_stream const &) noexcept
+            -> movable_stream & = delete;
+
+    movable_stream(movable_stream &&) noexcept
+        : input_buffer()
+        , output_buffer()
+    {
+    }
+    auto operator=(movable_stream &&) noexcept -> movable_stream &
+    {
+        return *this;
+    }
+
+private:
+    auto do_require_input(input_buffer::size_type) noexcept
+            -> dp::result<void> override
+    {
+        return dp::errc::bad;
+    }
+    auto do_discard_input(input_buffer::size_type) noexcept
+            -> dp::result<void> override
+    {
+        return dp::errc::bad;
+    }
+    auto do_bulk_read(std::byte *, std::size_t) noexcept
+            -> dp::result<void> override
+    {
+        return dp::errc::bad;
+    }
+
+    auto do_grow(output_buffer::size_type) noexcept -> dp::result<void> override
+    {
+        return dp::errc::bad;
+    }
+    auto do_bulk_write(std::byte const *, std::size_t) noexcept
+            -> dp::result<void> override
+    {
+        return dp::errc::bad;
+    }
+};
+
+} // namespace
+
+static_assert(dp::input_stream<movable_stream>);
+static_assert(dp::input_stream<movable_stream &>);
+static_assert(!dp::output_stream<movable_stream>);
+static_assert(dp::output_stream<movable_stream &>);
+
+TEST_CASE("movable_stream can be passed as lvalue to encode")
+{
+    movable_stream stream{};
+    CHECK(dp::encode(stream, dp::null_value).error() == dp::errc::bad);
+}
+
+TEST_CASE("movable_stream can be passed as lvalue to decode")
+{
+    movable_stream stream{};
+    CHECK(dp::decode(dp::as_value<int>, stream).error()
+          == dp::errc::end_of_stream);
+}
+TEST_CASE("movable_stream can be passed as rvalue to decode")
+{
+    CHECK(dp::decode(dp::as_value<int>, movable_stream{}).error()
+          == dp::errc::end_of_stream);
+}
 
 } // namespace dp_tests
